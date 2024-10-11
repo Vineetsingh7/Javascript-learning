4c0c5a0de1c3f7bf07c38c107261c1f9a7e3888e
vineet <mrsingh721767570000@gmail.com>
2024-10-11 12:31:34 +0530

    dom i javascript.

---
+++ Miniweb.js
@@ -0,0 +1,6 @@
+const changeBodyYellow = () =>{
+    document.body.firstElementChild.Style.backgorund = "yellow"
+}
+let b =document.body 
+console.log("First child of b is :",b.firstChild)
+console.log("First element child of b is :",b.firstElementChild)
